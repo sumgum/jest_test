b54881b115cc09c6b890c93ce8fce1b95f6871b2
Sumi Gunyu <sumkakkyo@gmail.com>
2020-08-07 23:17:39 +0900

    first demo test

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,5 @@
-const { TestScheduler } = require("jest");
-
 const sum = require('./sum');
 
-TestScheduler('adds 1 + 2 to equal 3', () => {
+test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
